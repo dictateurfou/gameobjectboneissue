19587fe318f61869ff7ab287327fd1145f69edc1
dictateurfou <stevenhervy@gmail.com>
2024-03-15 21:58:30 +0100

    Ajoutez des fichiers projet.

---
+++ code/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Mover : Component
+{
+	[Property] public float Speed { get; set; }
+	
+	protected override void OnFixedUpdate()
+	{
+		var transform = GameObject.Transform;
+		transform.Position += Transform.Rotation.Forward * Speed;
+	}
+}
---
+++ code/SphereComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class SpehereComponent : Component
+{
+	[Property] public float Radius { get; set; }
+
+	[Property] public Color Color { get; set; }
+	//OnPreRender
+	protected override void OnUpdate()
+	{
+		var draw = Gizmo.Draw;
+		draw.Color = Color;
+		using ( Gizmo.Scope("spherecom" + GameObject.GetHashCode() ) )
+		{
+			draw.SolidSphere( GameObject.Transform.Position, Radius );
+		}
+	}
+}
